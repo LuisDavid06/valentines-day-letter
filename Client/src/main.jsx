7bdcc1634a9bb81b4cfb93c2fbe6e8cc939a7a7c
lisandroviedo <lisandroviedo@hotmail.com>
2025-02-01 17:03:22 -0400

    Feat: Añadida función disableReactDevTools para ocultar información a herramientas de desarrollo de React
    Feat: Añadido logo de cargando con Suspense

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import "./index.css";
+import App from "./App.jsx";
+import { Label } from "./components/UI";
 
-createRoot(document.getElementById('root')).render(
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Suspense
+      fallback={
+        <div className="h-screen w-screen flex flex-col justify-center items-center">
+          <img src="./Loading.svg" alt="Loading" className="w-1/12" />
+          <Label className="font-bold">Cargando</Label>
+        </div>
+      }
+    >
+      <App />
+    </Suspense>
+  </StrictMode>
+);
