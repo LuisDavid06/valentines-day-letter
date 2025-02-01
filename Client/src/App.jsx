cd11112386595b7ca8c5d6ab01f445c4610d7224
lisandroviedo <lisandroviedo@hotmail.com>
2025-02-01 17:46:56 -0400

    Feat: Creación de prototipo de carta

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,8 +4,10 @@ import { Container, Button } from "./components/UI";
 
 function App() {
   return (
-    <Container>
-      <Button className="text-yellow-300">Prueba</Button>
+    <Container className="justify-center">
+      <div className="w-[30%] h-48 bg-white rounded-md flex items-center justify-center border-gray-300 border shadow-lg">
+        <img className="w-8" src="/favicon.svg" alt="" />
+      </div>
     </Container>
   );
 }
