76d9da056e28a43394bc547fe9bd1a3d294582b5
LisandrOviedo <lisandroviedo@hotmail.com>
2025-02-04 09:59:22 -0400

    Style: Añadido padding top al container para mejorar la ubicación de los elementos en cualquier tipo de pantalla. Añadido justify-between para ubicar los elementos de forma correcta junto con el footer.

--- Client/src/components/UI/Container.jsx
+++ Client/src/components/UI/Container.jsx
@@ -9,7 +9,7 @@ export const Container = ({
 }) => {
   return (
     <div
-      className={`flex h-screen flex-1 flex-col items-center px-6 lg:px-8 ${
+      className={`flex h-screen pt-[25vh] flex-1 flex-col items-center px-6 lg:px-8 justify-between ${
         blockEvents && "opacity-50 pointer-events-none"
       } ${className}`}
       {...props}
